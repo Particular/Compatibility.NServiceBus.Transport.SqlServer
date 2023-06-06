d0aaef080e96d72629144817674afcdb9acb7f64
Ramon Smits <ramon.smits@gmail.com>
2023-06-06 12:40:19 +0200

    Envvars made uppercase to follow Linux convention.

--- src/SqlServer.Tests/Global.cs
+++ src/SqlServer.Tests/Global.cs
@@ -2,5 +2,5 @@
 
 static class Global
 {
-    public static string ConnectionString { get; } = Environment.GetEnvironmentVariable("SqlServerTransportConnectionString") ?? "Data source = (local); Initial catalog = nservicebus; Integrated Security = true; Encrypt=false";
+    public static string ConnectionString { get; } = Environment.GetEnvironmentVariable("SQLSERVERTRANSPORTCONNECTIONSTRING") ?? "Data source = (local); Initial catalog = nservicebus; Integrated Security = true; Encrypt=false";
 }
