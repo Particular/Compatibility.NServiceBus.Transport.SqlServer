d3b3a3866899332538deeb929f61a794d65cc7ae
Szymon Pobiega <szymon.pobiega@gmail.com>
2023-06-27 17:53:47 +0200

    Use encryption in the connection string

--- src/SqlServer.Tests.Sources/Global.cs
+++ src/SqlServer.Tests.Sources/Global.cs
@@ -2,6 +2,6 @@
 
 static class Global
 {
-    public static string ConnectionString { get; } = Environment.GetEnvironmentVariable("SQLSERVERTRANSPORTCONNECTIONSTRING") ?? "Data source = (local); Initial catalog = nservicebus; Integrated Security = true; Encrypt=false";
+    public static string ConnectionString { get; } = Environment.GetEnvironmentVariable("SqlServerTransportConnectionString") ?? @"Data Source=.\SQLEXPRESS;Initial Catalog=nservicebus;Integrated Security=True";
 }
 
