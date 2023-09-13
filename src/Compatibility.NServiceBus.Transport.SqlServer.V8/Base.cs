bcd2b7369bf5f404f605e0d5527d2569e4fb4c7a
Andreas Öhlund <andreas.ohlund@particular.net>
2023-09-13 12:38:42 +0200

    Update to core 9 and sqlt 8

---
+++ src/Compatibility.NServiceBus.Transport.SqlServer.V8/Base.cs
@@ -0,0 +1,35 @@
+﻿using NServiceBus;
+using NServiceBus.Compatibility;
+
+abstract class Base : Plugin
+{
+    protected override EndpointConfiguration Configure(PluginOptions opts)
+    {
+        var endpointName = GetType().Name;
+
+        var config = new EndpointConfiguration(opts.ApplyUniqueRunPrefix(endpointName));
+
+        var transport = new SqlServerTransport(opts.ConnectionStrings[endpointName])
+        {
+            //TransportTransactionMode = TransportTransactionMode.ReceiveOnly,
+            TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive,
+        };
+
+        transport.Subscriptions.SubscriptionTableName = new NServiceBus.Transport.SqlServer.SubscriptionTableName(opts.ApplyUniqueRunPrefix("SubscriptionRouting"));
+
+        var routingConfig = config.UseTransport(transport);
+
+        Configure(opts, config, transport, routingConfig);
+
+        return config;
+    }
+
+    protected virtual void Configure(
+        PluginOptions opts,
+        EndpointConfiguration endpointConfig,
+        SqlServerTransport transportConfig,
+        RoutingSettings<SqlServerTransport> routingConfig
+        )
+    {
+    }
+}
---
+++ src/Compatibility.NServiceBus.Transport.SqlServer.V8/RequestResponse/Simple.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Compatibility;
+
+class Sender : Base
+{
+    protected override void Configure(
+        PluginOptions opts,
+        EndpointConfiguration endpointConfig,
+        SqlServerTransport transportConfig,
+        RoutingSettings<SqlServerTransport> routingConfig
+    )
+    {
+        routingConfig.RouteToEndpoint(typeof(MyRequest), opts.ApplyUniqueRunPrefix(nameof(Receiver)));
+    }
+
+    protected override async Task Execute(IEndpointInstance endpointInstance, CancellationToken cancellationToken = default)
+    {
+        await endpointInstance.Send(new MyRequest(), cancellationToken).ConfigureAwait(false);
+    }
+
+    public class MyResponseHandler : IHandleMessages<MyResponse>
+    {
+        public Task Handle(MyResponse message, IMessageHandlerContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
+
+class Receiver : Base
+{
+    public class MyRequestHandler : IHandleMessages<MyRequest>
+    {
+        public Task Handle(MyRequest message, IMessageHandlerContext context)
+        {
+            return context.Reply(new MyResponse());
+        }
+    }
+}
