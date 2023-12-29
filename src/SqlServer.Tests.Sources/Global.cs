0b7e54e9092c6dbae5d0f9936bc57c45e51afa0d
Ramon Smits <ramon.smits@gmail.com>
2023-12-29 13:57:32 +0100

    Added `TrustServerCertificate=True`  to fix startup error with SQLT 8.0.0-alpha.*

--- src/SqlServer.Tests.Sources/Global.cs
+++ src/SqlServer.Tests.Sources/Global.cs
@@ -2,5 +2,5 @@
 
 static class Global
 {
-    public static string ConnectionString { get; } = Environment.GetEnvironmentVariable("SqlServerTransportConnectionString") ?? @"Data Source=.\SQLEXPRESS;Initial Catalog=nservicebus;Integrated Security=True";
+    public static string ConnectionString { get; } = Environment.GetEnvironmentVariable("SqlServerTransportConnectionString") ?? @"Data Source=.\SQLEXPRESS;Initial Catalog=nservicebus;Integrated Security=True;TrustServerCertificate=True";
 }
